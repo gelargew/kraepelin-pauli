91c16c45a8888261219e828ec09b58aa70d554dd
gelargew <gelargew@gmail.com>
2021-05-26 17:59:55 +0700

    done with kraepelin logics

--- app/src/components/Kraepelin.js
+++ app/src/components/Kraepelin.js
@@ -3,13 +3,14 @@ import { ToggleTheme } from './toggleTheme'
 
 
 export const Kraepelin = ({
-    kraepelinLength = 5000
+    kraepelinLength = 5000,
+    columnCount = 100
 }) => {
-    const [number , setNumber] = useState(randomArray({length: kraepelinLength}))
-    const [answers, setAnswers] = useState(new Array(100))
-    const [result, setResult] = useState(new Array(100))
+    const [numbers , setNumbers] = useState(randomArray({length: kraepelinLength + 1}))
+    const [answers, setAnswers] = useState(new Array(kraepelinLength))
+    const [result, setResult] = useState(new Array(kraepelinLength))
     const [position, setPosition] = useState(0)
-    const [curNumber, setCurNumber] = useState(number.slice(0, 20))
+    const [curNumbers, setCurNumbers] = useState(numbers.slice(0, columnCount))
     const container = useRef(null)
     const kraepelinInputs = useRef(null)
     const [inputDisabled, setInputDisabled] = useState(true)
@@ -21,22 +22,32 @@ export const Kraepelin = ({
     }, [])
 
     useEffect( () => {
-        container.current.style.transform = `translateY(-${position % (curNumber.length - 1)}00px)`
-        if (position > 0 && position % (curNumber.length - 1) == 0) {
-            setCurNumber(number.slice(position, position + curNumber.length))
+        container.current.style.transform = `translateY(-${position % (curNumbers.length - 1)}00px)`
+        if (position >= kraepelinLength) {
+            setPosition(0)
+            setCurNumbers(numbers.slice(0, columnCount))
         }
-        setTimeout(() => setInputDisabled(false), 300)
+        else if (position > 0 && position % (curNumbers.length - 1) == 0) {
+            setCurNumbers(numbers.slice(position, position + columnCount))
+        }
+        setTimeout(() => setInputDisabled(false), 0)
     },[position])
 
     const handleInput = async e => {
         setInputDisabled(true)
         const val = e ? parseInt(e.target.value) : '0'
-        setPosition(prev => prev + 1)
-        setAnswers(prev => {
-            prev[position] = val
-            console.log(answers[position])
-            return prev
-        })
+        if (handleDown()) {
+            setAnswers(prev => {
+                prev[position] = val
+                console.log(answers[position])
+                return prev
+            })
+            setResult(prev => {
+                const correct = (numbers[position] + numbers[position + 1]) % 10 === val
+                prev[position] = correct ? 1 : -1
+                return prev
+            })
+        }
     }
 
     const handleKeyup = e => {
@@ -52,21 +63,34 @@ export const Kraepelin = ({
             if (e.code === 'ArrowUp') clickInput(9)
             else if (e.code === 'ArrowDown') clickInput(11)
         }
-
     }
 
     const clickInput = idx => kraepelinInputs.current.children[idx].click()
+    const handleUp = () => {
+        if (position < 1) return;
+        setPosition(prev => prev - 1)
+    }
+    const handleDown = () => {
+        if (position >= kraepelinLength) {
+            return false
+        }
+        setPosition(prev => prev + 1)
+        return true
+    }
 
     return (
         <div className='kraepelin' >
-            <ToggleTheme />
+            <button onClick={() => {
+                console.log(answers)
+                console.log(result)
+            }}>submit</button>
             <div className='container' >
                 <div className={inputDisabled ? 'answer-line flash' : 'answer-line'}></div>
                 <div className='kraepelin-numbers' ref={container}>
-                    {curNumber.map((l, i) =>                  
+                    {curNumbers.map((l, i) =>                  
                     <li key={i}>
-                        {l}
-                        <p>{answers[parseInt(position/curNumber.length)*(curNumber.length - 1) + i]}</p>
+                        {l}-{i}
+                        <p>{answers[parseInt(position/curNumbers.length)*(curNumbers.length - 1) + i]} {i}</p>
                     </li>
                     )}
                 </div>
@@ -78,11 +102,11 @@ export const Kraepelin = ({
                 className='kraepelin-input' value={num} disabled={inputDisabled}>
                     {num}
                 </button>)}
-                <button className='kraepelin-input' onClick={() => setPosition(prev => prev - 1)} disabled={inputDisabled}>
+                <button className='kraepelin-input' onClick={handleUp} disabled={inputDisabled}>
                     <i className="fas fa-chevron-up fa-2x"></i>
                 </button>
                 <button className='kraepelin-input' onClick={handleInput} value="0" disabled={inputDisabled}>0</button>
-                <button className='kraepelin-input' onClick={() => setPosition(prev => prev + 1)} disabled={inputDisabled}>
+                <button className='kraepelin-input' onClick={handleDown} disabled={inputDisabled}>
                     <i className="fas fa-chevron-down fa-2x"></i>
                 </button>
             </div>
