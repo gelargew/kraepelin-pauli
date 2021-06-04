8a8efbfdfbe0606f2e10321fd07d53a4cdbf49a2
gelargew <gelargew@gmail.com>
2021-06-04 18:45:49 +0700

    push heroku

--- kraepelin/settings.py
+++ kraepelin/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
